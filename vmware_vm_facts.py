8a6f5b0831da72d4c8826e4415e99b64e5110d0d
Ryan Hennessy <rhenness@redhat.com>
2017-01-05 10:48:54 -0600

    Added the changes that comment out the exception statements

--- vmware_vm_facts.py
+++ vmware_vm_facts.py
@@ -87,10 +87,10 @@ def main():
         content = connect_to_api(module)
         _virtual_machines = get_all_virtual_machines(content)
         module.exit_json(changed=False, virtual_machines=_virtual_machines)
-    except vmodl.RuntimeFault as runtime_fault:
-        module.fail_json(msg=runtime_fault.msg)
-    except vmodl.MethodFault as method_fault:
-        module.fail_json(msg=method_fault.msg)
+#    except vmodl.RuntimeFault as runtime_fault:
+#        module.fail_json(msg=runtime_fault.msg)
+#    except vmodl.MethodFault as method_fault:
+#        module.fail_json(msg=method_fault.msg)
     except Exception as e:
         module.fail_json(msg=str(e))
 
